78ecf612d2cf534c899481860828248df377bf26
bar-benjamin <bar.benjamin123@gmail.com>
2026-03-09 12:04:00 +0200

    first commit

---
+++ src/main/java/com/ecommerce/scalableecommerceplatform/ScalableEcommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.scalableecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableEcommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalableEcommercePlatformApplication.class, args);
+    }
+
+}
